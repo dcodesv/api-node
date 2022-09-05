3d20d7091bead5efccc6701c7d040fc86025435b
Diego Armando Villalobos Villalta <d_villalobos@outlook.com>
2022-09-04 21:34:23 -0600

    Cambio en endpoint para obtener denuncias

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,7 +2,7 @@ const express =  require('express');
 const router = express.Router();
 const mysqlConnection = require('../database');
 
-router.get('/denuncias', (req, res)=>{
+router.get('/obtener-denuncias', (req, res)=>{
     mysqlConnection.query('SELECT * FROM Denucias',(err,rows, fields)=>{
         if(!err){
             res.json(rows);
